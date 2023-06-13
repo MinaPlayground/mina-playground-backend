a9163ec7a110dbf4d52f5d4d5f145f76e0fa1618
devarend <devarend@gmail.com>
2023-06-13 17:37:11 +0200

    project controller which uses the filetree service and adds a filesystemtree with a reference to the project

---
+++ src/project/project.controller.ts
@@ -0,0 +1,47 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Project } from './schemas/project.schema';
+import { CreateProjectDTO } from './dto/create-project.dto';
+import { ProjectService } from './project.service';
+import { FileTreeService } from '../filetree/filetree.service';
+
+@Controller('project')
+export class ProjectController {
+  constructor(
+    private readonly projectService: ProjectService,
+    private readonly fileTreeService: FileTreeService,
+  ) {}
+
+  @Post()
+  async create(@Body() createProjectDTO: CreateProjectDTO) {
+    const item = await this.projectService.create(createProjectDTO);
+    await this.fileTreeService.create({
+      project_id: item._id,
+      fileSystemTree: {
+        test: {
+          directory: {
+            'testFile.js': {
+              file: {
+                contents: 'testtesttest',
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+
+  @Get()
+  async findAll(): Promise<Project[]> {
+    return this.projectService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Project> {
+    return this.projectService.findOne(id);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.projectService.delete(id);
+  }
+}
