881ce16d404702565ee087fb3c711a3ad627bcf6
devarend <devarend@gmail.com>
2023-06-20 21:59:23 +0200

    implement template for zkapp

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -3,6 +3,7 @@ import { Project } from './schemas/project.schema';
 import { CreateProjectDTO } from './dto/create-project.dto';
 import { ProjectService } from './project.service';
 import { FileTreeService } from '../filetree/filetree.service';
+import { zkAppTemplate } from '../templates/zkApp';
 
 @Controller('project')
 export class ProjectController {
@@ -16,50 +17,7 @@ export class ProjectController {
     const item = await this.projectService.create(createProjectDTO);
     await this.fileTreeService.create({
       project_id: item._id,
-      fileSystemTree: {
-        test: {
-          directory: {
-            'testFile*js': {
-              file: {
-                contents:
-                  'import { Field, SmartContract, state, State, method } from "snarkyjs";\n' +
-                  '// test\n' +
-                  'export class Add extends SmartContract {\n' +
-                  '  @state(Field) num = State<Field>();\n' +
-                  '\n' +
-                  '  init() {\n' +
-                  '    super.init();\n' +
-                  '    this.num.set(Field(1));\n' +
-                  '  }\n' +
-                  '\n' +
-                  '  @method update() {\n' +
-                  '    const currentState = this.num.getAndAssertEquals();\n' +
-                  '  }\n' +
-                  '}\n',
-              },
-            },
-            'testFile2*js': {
-              file: {
-                contents:
-                  'import { Field, SmartContract, state, State, method } from "snarkyjs";\n' +
-                  '// test\n' +
-                  'export class Add extends SmartContract {\n' +
-                  '  @state(Field) num = State<Field>();\n' +
-                  '\n' +
-                  '  init() {\n' +
-                  '    super.init();\n' +
-                  '    this.num.set(Field(1));\n' +
-                  '  }\n' +
-                  '\n' +
-                  '  @method update() {\n' +
-                  '    const currentState = this.num.getAndAssertEquals();\n' +
-                  '  }\n' +
-                  '}\n',
-              },
-            },
-          },
-        },
-      },
+      fileSystemTree: zkAppTemplate,
     });
   }
 
