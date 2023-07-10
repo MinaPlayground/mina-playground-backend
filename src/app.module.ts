c16fb1dd0d593f8776a8b12a26742be651c9619a
devarend <devarend@gmail.com>
2023-07-11 01:40:33 +0200

    added nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FileTreeModule } from './filetree/filetree.module';
+import { ProjectModule } from './project/project.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb+srv://arnout:<password>@minaplayground.il3cad4.mongodb.net/?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/?retryWrites=true&w=majority`,
     ),
     FileTreeModule,
+    ProjectModule,
   ],
 })
 export class AppModule {}
